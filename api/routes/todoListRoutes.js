bc26b54370b5b7e2ea45ccecc8e9a036e8f6640e
Fotis Pegios <fotispeg@gmail.com>
2018-06-20 14:21:44 +0300

    insert comments

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -4,12 +4,12 @@ module.exports = function(app) {
 
   // todoList Routes
   app.route('/tasks')
-    .get(todoList.list_all_tasks)
-    .post(todoList.create_a_task);
+    .get(todoList.list_all_tasks) // read all
+    .post(todoList.create_a_task); // create one
 
 
   app.route('/tasks/:taskId')
-    .get(todoList.read_a_task)
-    .put(todoList.update_a_task)
-    .delete(todoList.delete_a_task);
+    .get(todoList.read_a_task) // read one
+    .put(todoList.update_a_task) // update one
+    .delete(todoList.delete_a_task); // delete one
 };
